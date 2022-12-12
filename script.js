1d8228616535f90b053ce96fc3bed2e0696c99c4
Iurii <artcybashev@gmail.com>
2022-12-12 17:48:26 -0600

    Created FOR Loop and Statements IF for questions. Date Dec 12, 2022.

--- script.js
+++ script.js
@@ -9,13 +9,37 @@ const personalMoviesDB ={
     privat:false            
 };
 
-const a = prompt('What last movie did you watch?', ""),
-      b = prompt('What is your rate?', ""),
-      c = prompt('What last movie did you watch?', ""),
-      d = prompt('What is your rate?', "");  
+// const a = prompt('What last movie did you watch?', ""),
+//       b = prompt('What is your rate?', ""),
+//       c = prompt('What last movie did you watch?', ""),
+//       d = prompt('What is your rate?', "");  
       
-personalMoviesDB.movies[a] = b;
-personalMoviesDB.movies[c] = d;
+// personalMoviesDB.movies[a] = b;
+//personalMoviesDB.movies[c] = d;
+
+for (let i =0; i<2; i++) {
+    const a = prompt('What last movie did you watch?', ""),
+          b = prompt('What is your rate?', "");
+    if(a != null && b !=null && a != '' && b !="" && a.length <=50) {
+        personalMoviesDB.movies[a] = b;
+        console.log('done');
+          
+          }else {
+            i--;
+          }
+}
+
+if (personalMoviesDB.count <10){
+    console.log('Too small');
+
+}else if(personalMoviesDB.count > 10 && personalMoviesDB.count < 30) {
+    console.log('You are in the middle');
+}else if (personalMoviesDB.count >30) {
+    console.log ('You are supergood');
+}else {
+    console.log('Error');
+}
+
 
 console.log(personalMoviesDB);
 
